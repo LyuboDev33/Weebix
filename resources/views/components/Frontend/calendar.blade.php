1fadaf5cf064a58eceb3a6e6481c5448076abb85
user <user@192.168.1.3>
2026-05-18 18:20:20 +0300

    Chaning the Cal ID

--- resources/views/components/Frontend/calendar.blade.php
+++ resources/views/components/Frontend/calendar.blade.php
@@ -4,55 +4,69 @@
         let p = function(a, ar) {
             a.q.push(ar);
         };
+
         let d = C.document;
+
         C.Cal = C.Cal || function() {
             let cal = C.Cal;
             let ar = arguments;
+
             if (!cal.loaded) {
                 cal.ns = {};
                 cal.q = cal.q || [];
-                d.head.appendChild(d.createElement("script")).src = A;
+
+                d.head
+                    .appendChild(d.createElement("script"))
+                    .src = A;
+
                 cal.loaded = true;
             }
+
             if (ar[0] === L) {
+
                 const api = function() {
                     p(api, arguments);
                 };
+
                 const namespace = ar[1];
+
                 api.q = api.q || [];
+
                 if (typeof namespace === "string") {
-                    cal.ns[namespace] = cal.ns[namespace] || api;
+
+                    cal.ns[namespace] =
+                        cal.ns[namespace] || api;
+
                     p(cal.ns[namespace], ar);
                     p(cal, ["initNamespace", namespace]);
-                } else p(cal, ar);
+
+                } else {
+                    p(cal, ar);
+                }
+
                 return;
             }
+
             p(cal, ar);
         };
+
     })(window, "https://app.cal.com/embed/embed.js", "init");
-    Cal("init", "izrabotka-website", {
-        origin: "https://app.cal.com",
-        locale: 'bg'
+
+    Cal("init", "30min", {
+        origin: "https://app.cal.com"
     });
 
-
-    // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
-    // `data-cal-link="lyubomir-stoyanov-jzm3o7/izrabotka-website"`
-    // data-cal-namespace="izrabotka-website"
-    // `data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'`
-
-    Cal.ns["izrabotka-website"]("ui", {
-        "hideEventTypeDetails": false,
-        "layout": "month_view"
+    Cal.ns["30min"]("ui", {
+        hideEventTypeDetails: false,
+        layout: "month_view"
     });
 </script>
 <!-- Cal element-click embed code ends -->
-<a data-cal-link="lyubomir-stoyanov-jzm3o7/izrabotka-website"
-    data-cal-namespace="izrabotka-website"
-    data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'
-    class="btn-default cursor-pointer d-block w-fit m-0-auto"
-    onclick="fbq('track', 'Lead');">
 
+
+<a data-cal-link="nikolay-dobrev-b0eyt2/30min" data-cal-namespace="30min"
+    data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'
+    class="btn-default cursor-pointer d-block w-fit m-0-auto" onclick="fbq('track', 'Lead');">
     Запази Безплатна консултация
     <i class="fa-solid fa-calendar-days"></i>
 </a>
